107e851bea8deca0174c386d29abfaadaac51a4c
BleskoDev <bleskodev@gmail.com>
2017-01-18 11:43:15 +0100

    Bring misc.c under test harness.
    misc.c is now part of siggen_common library.

---
+++ src/misc.h
@@ -0,0 +1,67 @@
+/* misc.h
+ * Miscellaneous Functions 
+ * Jim Jackson     Dec 96
+ */
+
+/*
+ * Copyright (C) 1997-2008 Jim Jackson                    jj@franjam.org.uk
+ * Copyright (C) 2017      Blesko Dev                     bleskodev@gmail.com
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program - see the file COPYING; if not, write to 
+ *  the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, 
+ *  MA 02139, USA.
+ */
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * delay(us)  wait us microsecs using select. Effectively
+ *            causes current process to stop and a reschedule take place
+ *            as well as doing the delay.
+ */
+int delay(int us);
+
+/* 
+ * err_rpt(err,msg)
+ */
+int err_rpt(short int err, char *msg);
+
+/*
+ * hcf(x,y)   find highest common factor of x and y
+ */
+int hcf(unsigned x, unsigned y);
+
+/*
+ * parse(s,aa,sep) splits s up into parameters and stores ptrs
+ *                 to each prm in ptr array aa (upto MAX_ARGS)
+ *                 params are space or tab or 'sep' seperated, and may be
+ *                 enclosed in single or double quotes e.g. 'this is 1 prm'
+ *
+ * returns number of parameters parsed
+ */
+enum { MAX_ARGS = 50 };
+int parse(char *s,char **aa, char sep);
+
+/*
+ * mstosamples(ms,sr)  convert ms millisecs into number of samples
+ *                     given that the sample rate is sr samples/sec
+ */
+int mstosamples(int ms, int sr);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
---
+++ test/test_misc.cpp
@@ -0,0 +1,100 @@
+#include <gmock/gmock.h>
+#include "src/misc.h"
+
+// err_rpt() requires extern char* sys to be defined somewhere
+// TODO: remove this requirement
+char* sys;
+
+TEST(misc, hcf)
+{
+  EXPECT_EQ(6, hcf(54, 24));
+  EXPECT_EQ(14, hcf(42, 56));
+
+  EXPECT_EQ(1, hcf(5, 7));
+
+  // TODO: handle O argument correctly
+  //EXPECT_EQ(6, hcf(0, 6));
+}
+
+//**************************************************************
+TEST(misc, parse_1_param)
+{
+  char* output[MAX_ARGS];
+  char sep = ' ';
+  char input[] = "parameter";
+  const int paramCount = parse(input, output, sep);
+  EXPECT_EQ(1, paramCount);
+  EXPECT_EQ(output[0], input);
+}
+
+TEST(misc, parse_1_param_with_quotes)
+{
+  char* output[MAX_ARGS];
+  char sep = ' ';
+  char input[] = "'this is one parameter'";
+  const int paramCount = parse(input, output, sep);
+  EXPECT_EQ(1, paramCount);
+  EXPECT_EQ(output[0], input);
+}
+
+TEST(misc, parse_2_params)
+{
+  char* output[MAX_ARGS];
+  char sep = ' ';
+  char input[] = "parameter1 parameter2";
+  const char* expectedParam1 = input;
+  const char* expectedParam2 = strchr(input, sep) + 1;
+  const int paramCount = parse(input, output, sep);
+  EXPECT_EQ(2, paramCount);
+  EXPECT_EQ(output[0], expectedParam1);
+  EXPECT_EQ(output[1], expectedParam2);
+}
+
+TEST(misc, parse_2_params_with_quotes)
+{
+  char* output[MAX_ARGS];
+  char sep = ' ';
+  char input[] = "parameter1 'this is parameter2'";
+  const char* expectedParam1 = input;
+  const char* expectedParam2 = strchr(input, sep) + 1;
+  const int paramCount = parse(input, output, sep);
+  EXPECT_EQ(2, paramCount);
+  EXPECT_EQ(output[0], expectedParam1);
+  EXPECT_EQ(output[1], expectedParam2);
+}
+
+TEST(misc, parse_with_custom_separator)
+{
+  char* output[MAX_ARGS];
+  char sep = '_';
+  char input[] = "parameter1_parameter2";
+  const char* expectedParam1 = input;
+  const char* expectedParam2 = strchr(input, sep) + 1;
+  const int paramCount = parse(input, output, sep);
+  EXPECT_EQ(2, paramCount);
+  EXPECT_EQ(output[0], expectedParam1);
+  EXPECT_EQ(output[1], expectedParam2);
+}
+
+TEST(misc, parse_ignores_leading_separators)
+{
+  char* output[MAX_ARGS];
+  char sep = '_';
+  char input[] = " \t_parameter1_ \tparameter2";
+  const char* expectedParam1 = strchr(input, 'p');
+  const char* expectedParam2 = strchr(expectedParam1 + 1, 'p');
+  const int paramCount = parse(input, output, sep);
+  EXPECT_EQ(2, paramCount);
+  EXPECT_EQ(output[0], expectedParam1);
+  EXPECT_EQ(output[1], expectedParam2);
+}
+
+//**************************************************************
+TEST(misc, mstosamples)
+{
+  EXPECT_EQ(48000, mstosamples(1000, 48000));
+  EXPECT_EQ(44100, mstosamples(1000, 44100));
+
+  EXPECT_EQ(48, mstosamples(1, 48000));
+  EXPECT_EQ(44, mstosamples(1, 44100));
+}
