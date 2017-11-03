b5e09e465213f20deb39777639d0e2789d1406bd
BleskoDev <bleskodev@gmail.com>
2017-11-03 19:57:16 +0100

    Additional tests for generator.c.

--- test/test_generator.cpp
+++ test/test_generator.cpp
@@ -1,6 +1,7 @@
 #include <gmock/gmock.h>
 #include "src/generator.h"
 #include <linux/soundcard.h>
+#include <cstring>
 
 // vflg is required for generator.c to compile.
 // TODO: remove this requirement.
@@ -108,6 +109,7 @@ TEST(generator, chanmix_16bit)
 }
 */
 
+//**************************************************************
 TEST(generator, do_antiphase_8bits)
 {
   unsigned char input[] = { 25, 15, 255, 128 };
@@ -142,3 +144,147 @@ TEST(generator, do_antiphase_16bits)
   }
 }
 
+//**************************************************************
+TEST(generator, generate_fails_invalid_waveform)
+{
+  char wf[] = "toto";
+  EXPECT_EQ(0, generate(wf, nullptr, 100, 50, 50, 100, 0, AFMT_U8));
+}
+
+TEST(generator, generate_fails_frequency_too_high_for_samples)
+{
+  char wf[] = "sine";
+  unsigned int samplesPerSec = 100;
+  unsigned int freq = 51;
+  EXPECT_EQ(0, generate(wf, nullptr, samplesPerSec, freq, 50, samplesPerSec, 0, AFMT_U8));
+}
+
+//**************************************************************
+TEST(generator, generate_silence_8bits)
+{
+  char wf[] = "off";
+  const unsigned int samplesPerSec = 10;
+  const unsigned int freq = 5;
+  const unsigned int ampl = 50;
+  char buff[samplesPerSec];
+  std::memset(buff, 0xcd, samplesPerSec);
+
+  EXPECT_EQ(samplesPerSec, generate(wf, buff, samplesPerSec, freq, ampl, samplesPerSec, 0, AFMT_U8));
+  for (auto val : buff)
+  {
+    EXPECT_EQ(char(128), val);
+  }
+}
+
+TEST(generator, generate_silence_16bits)
+{
+  char wf[] = "off";
+  const unsigned int samplesPerSec = 10;
+  const unsigned int bufferSize = samplesPerSec * 2;
+  const unsigned int freq = 5;
+  const unsigned int ampl = 50;
+  char buff[bufferSize];
+  std::memset(buff, 0xcd, bufferSize);
+
+  EXPECT_EQ(samplesPerSec, generate(wf, buff, bufferSize, freq, ampl, samplesPerSec, 0, AFMT_S16_LE));
+  for (auto val : buff)
+  {
+    EXPECT_EQ(char(0), val);
+  }
+}
+
+TEST(generator, generate_square_8bits)
+{
+  char wf[] = "square";
+  const unsigned int samplesPerSec = 100;
+  const unsigned int bufferSize = samplesPerSec;
+  const unsigned int freq = 1;
+  const unsigned int ampl = 70;
+  const unsigned int ratio = 50;
+  char buff[bufferSize];
+  char expected[bufferSize];
+  const char maxValue = 128 + ampl * 127 / 100;
+  const char minValue = 128 - ampl * 128 / 100;
+  for(int i = 0; i < bufferSize; ++i)
+  {
+    expected[i] = i < ratio ? maxValue : minValue;
+  }
+
+  EXPECT_EQ(samplesPerSec, generate(wf, buff, bufferSize, freq, ampl, samplesPerSec, 0, AFMT_U8));
+  for (int i = 0; i < bufferSize; ++i)
+  {
+    EXPECT_EQ(expected[i], buff[i]);
+  }
+}
+
+TEST(generator, generate_square_16bits)
+{
+  char wf[] = "square";
+  const unsigned int samplesPerSec = 100;
+  const unsigned int bufferSize = samplesPerSec * 2;
+  const unsigned int freq = 1;
+  const unsigned int ampl = 70;
+  const unsigned int ratio = 50;
+  char buff[bufferSize];
+  short int expected[samplesPerSec];
+  const short int value = ampl * 32767 / 100;
+  for(int i = 0; i < samplesPerSec; ++i)
+  {
+    expected[i] = i < ratio ? value : -value;
+  }
+
+  EXPECT_EQ(samplesPerSec, generate(wf, buff, bufferSize, freq, ampl, samplesPerSec, 0, AFMT_S16_LE));
+  short int* buffAsShort = reinterpret_cast<short int*>(buff);
+  for (int i = 0; i < samplesPerSec; ++i)
+  {
+    EXPECT_EQ(expected[i], buffAsShort[i]);
+  }
+}
+
+TEST(generator, generate_pulse_8bits)
+{
+  char wf[] = "pulse";
+  const unsigned int samplesPerSec = 100;
+  const unsigned int bufferSize = samplesPerSec;
+  const unsigned int freq = 1;
+  const unsigned int ampl = 50;
+  const unsigned int ratio = 40;
+  char buff[bufferSize];
+  char expected[bufferSize];
+  const char maxValue = 128 + ampl * 127 / 100;
+  const char minValue = 128 - ampl * 128 / 100;
+  for(int i = 0; i < bufferSize; ++i)
+  {
+    expected[i] = i < ratio ? maxValue : minValue;
+  }
+
+  EXPECT_EQ(samplesPerSec, generate(wf, buff, bufferSize, freq, ampl, samplesPerSec, ratio, AFMT_U8));
+  for (int i = 0; i < bufferSize; ++i)
+  {
+    EXPECT_EQ(expected[i], buff[i]);
+  }
+}
+
+TEST(generator, generate_pulse_16bits)
+{
+  char wf[] = "pulse";
+  const unsigned int samplesPerSec = 100;
+  const unsigned int bufferSize = samplesPerSec * 2;
+  const unsigned int freq = 1;
+  const unsigned int ampl = 50;
+  const unsigned int ratio = 40;
+  char buff[bufferSize];
+  short int expected[samplesPerSec];
+  const short int value = ampl * 32767 / 100;
+  for(int i = 0; i < samplesPerSec; ++i)
+  {
+    expected[i] = i < ratio ? value : -value;
+  }
+
+  EXPECT_EQ(samplesPerSec, generate(wf, buff, bufferSize, freq, ampl, samplesPerSec, ratio, AFMT_S16_LE));
+  short int* buffAsShort = reinterpret_cast<short int*>(buff);
+  for (int i = 0; i < samplesPerSec; ++i)
+  {
+    EXPECT_EQ(expected[i], buffAsShort[i]);
+  }
+}
